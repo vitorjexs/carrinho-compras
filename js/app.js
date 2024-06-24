0756808097ef4eab02d35b327694a36238d3d004
Vitor <vitortl@uol.com.br>
2024-06-24 12:04:41 -0300

    Lógica implantada para adicionar produto e multiplicar quantidade

---
+++ js/app.js
@@ -0,0 +1,73 @@
+
+let formulario = document.getElementById("produto");
+
+console.log(formulario);
+
+let fone;
+let celular;
+let oculus;
+
+let foneValor = 100;
+let celularValor = 1400;
+let oculusValor = 5000;
+
+// Função para verificar se contém
+
+function encontrar(opcao) {
+   
+   for (let i = 0; i < formulario.options.length; i++){
+      
+      let option = formulario.options[i];
+     
+      if (option.text.includes(opcao)){
+         console.log(`Achei ${opcao}`);
+         break;
+      } else {
+         console.log("Este produto não existe")
+      }
+   }
+
+}
+
+
+
+// função para adicionar ao carrinho
+function adicionar(){
+   let listaProdutos = document.getElementById("lista-produtos");
+
+   let item;
+   let produtoValor;
+   let total = document.getElementById("valor-total");
+      
+      
+   // // função para multiplicar o valor pela quantidade
+   // function multiplicar() {
+      
+      let quantidade = document.getElementById("quantidade");
+      
+
+      if (produto.value == "Fone de ouvido - R$100") {
+         item = "Fone de ouvido";
+         produtoValor = quantidade.value * foneValor;
+         console.log(produtoValor);
+      } else if (produto.value == "Celular - R$1400") {
+         item = "Celular";
+         produtoValor = quantidade.value * celularValor;
+      } else if (produto.value == "Oculus VR - R$5000") {
+         item = "Oculus VR";
+         produtoValor = quantidade.value * oculusValor;
+      }
+      
+      
+      
+   // }
+   
+   listaProdutos.innerHTML = `${quantidade.value}x ${item} R$ ${produtoValor}`;
+   total.innerHTML = `R$ ${produtoValor}`;
+}
+   
+// função para limpar
+function limpar(){
+   let listaProdutos = document.getElementById("lista-produtos");
+   listaProdutos.innerHTML= "Carrinho vazio";
+}
