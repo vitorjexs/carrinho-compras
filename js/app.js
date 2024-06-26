c3c404752b9e081400c8216d757721ae8fee58c3
Vitor <vitortl@uol.com.br>
2024-06-26 14:19:31 -0300

    Lista dinâmica, inicializaçao da pagina corrigida

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
 // PRIMEIRA LINHA
 
 let formulario = document.getElementById("produto");
+let listaProdutos = document.getElementById("lista-produtos");
+let total = document.getElementById("valor-total");
+let quantidade = document.getElementById('quantidade');
 
 let foneValor = 100;
 let celularValor = 1400;
 let oculusValor = 5000;
+let totalCarrinho = 0;
+
+//Inicialização da página
+listaProdutos.innerHTML = `
+      <section class="carrinho__produtos__produto">
+      <span class="texto-azul">Adicione seus produtos através da lista ao lado</span>
+      </section>`;
+total.textContent = `R$ 0`
 
 // Função para verificar se contém, apenas para visualizar no console
 
@@ -25,35 +36,49 @@ function encontrar(opcao) {
 
 // função para adicionar ao carrinho
 function adicionar() {
-   
+
    let item;
    let produtoValor;
-
-   let listaProdutos = document.getElementById("lista-produtos");
-   let total = document.getElementById("valor-total");
-   let quantidade = document.getElementById("quantidade");
+   let produtosTotal = 0;
 
    if (produto.value == "Fone de ouvido - R$100") {
       item = "Fone de ouvido";
       produtoValor = quantidade.value * foneValor;
-      console.log(produtoValor);
-   } else if (produto.value == "Celular - R$1400") {
+
+      produtosTotal = produtosTotal + produtoValor;
+
+      console.log("Total do Celular -> " + produtosTotal);
+
+   }
+   if (produto.value == "Celular - R$1400") {
       item = "Celular";
       produtoValor = quantidade.value * celularValor;
-   } else if (produto.value == "Oculus VR - R$5000") {
+      produtosTotal = produtosTotal + produtoValor;
+
+   }
+   if (produto.value == "Oculus VR - R$5000") {
       item = "Oculus VR";
       produtoValor = quantidade.value * oculusValor;
+      produtosTotal = produtosTotal + produtoValor;
+
    }
 
-   listaProdutos.innerHTML = `
+
+   listaProdutos.innerHTML = listaProdutos.innerHTML + `
       <section class="carrinho__produtos__produto">
-      <span class="texto-azul">${quantidade.value}x</span> ${item} <span class="texto-azul">${produtoValor}</span>
+      <span class="texto-azul">${quantidade.value}x</span> ${item} <span class="texto-azul">R$ ${produtoValor}</span>
       </section>`;
-   total.innerHTML = `R$ ${produtoValor}`;
+
+   totalCarrinho = totalCarrinho + produtosTotal;
+   total.textContent = `R$ ${totalCarrinho}`;
+   console.log(totalCarrinho);
 }
 
 // função para limpar
 function limpar() {
    let listaProdutos = document.getElementById("lista-produtos");
-   listaProdutos.innerHTML = `<span class="texto-azul"> Carrinho Vazio </span>`;
+   listaProdutos.innerHTML = `<section class="carrinho__produtos__produto">
+      <span class="texto-azul"> Carrinho Vazio </span> 
+      </section>`;
+   total.textContent = `R$ 0`
 }
